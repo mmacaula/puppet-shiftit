07121def14491543db5f27f6fdf11baba3aaac89
Mike Macaulay <mmacaula@gmail.com>
2015-07-30 14:58:45 -0500

    Update init.pp
    remove xquartz dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,6 @@
 #   include shiftit
 class shiftit {
 
-    include xquartz
-
     package { 'ShiftIt':
         provider => 'compressed_app',
         source   => 'https://github.com/downloads/fikovnik/ShiftIt/ShiftIt-develop-1.6.3.zip'
